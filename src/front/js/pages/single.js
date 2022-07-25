f0053384011bd5e267b7a8304edb35cdd92b7ee0
bpmurray77 <bpmurray77@gmail.com>
2022-07-25 23:50:25 +0000

    Styled and setup

--- src/front/js/pages/single.js
+++ src/front/js/pages/single.js
@@ -15,34 +15,91 @@ export const Single = props => {
   			<div class="accordion-item">
     			<h2 class="accordion-header" id="headingOne">
       			<button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-        			Stone Floor
+				  <a href="https://www.thingiverse.com/thing:171315/files">Stone Dungeon Floor</a>
       			</button>
     			</h2>
 				<div id="collapseOne" class="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
 					<div class="accordion-body">
 						<ul class="list-group">
-							<li class="list-group-item">2x2</li>
-							<li class="list-group-item">3x3</li>
-							<li class="list-group-item">4x4</li>
-							<li class="list-group-item">2x6</li>
-							<li class="list-group-item">6x6</li>
+							<li class="list-group-item">1x1
+								<div class="input-group">
+								<span class="input-group-text">Quantity</span>
+								<input type="number" aria-label="Quantity" class="form-control"/>
+								</div>
+							</li>
+							<li class="list-group-item">1x2
+								<div class="input-group">
+								<span class="input-group-text">Quantity</span>
+								<input type="number" aria-label="Quantity" class="form-control"/>
+								</div>
+							</li>
+							<li class="list-group-item">1x3
+								<div class="input-group">
+								<span class="input-group-text">Quantity</span>
+								<input type="number" aria-label="Quantity" class="form-control"/>
+								</div>
+							</li>
+							<li class="list-group-item">1x4
+								<div class="input-group">
+								<span class="input-group-text">Quantity</span>
+								<input type="number" aria-label="Quantity" class="form-control"/>
+								</div>
+							</li>
+							<li class="list-group-item">2x2
+								<div class="input-group">
+								<span class="input-group-text">Quantity</span>
+								<input type="number" aria-label="Quantity" class="form-control"/>
+								</div>
+							</li>
+							<li class="list-group-item">2x3
+								<div class="input-group">
+								<span class="input-group-text">Quantity</span>
+								<input type="number" aria-label="Quantity" class="form-control"/>
+								</div>
+							</li>
+							<li class="list-group-item">2x4
+								<div class="input-group">
+								<span class="input-group-text">Quantity</span>
+								<input type="number" aria-label="Quantity" class="form-control"/>
+								</div>
+							</li>
+							<li class="list-group-item">3x3
+								<div class="input-group">
+								<span class="input-group-text">Quantity</span>
+								<input type="number" aria-label="Quantity" class="form-control"/>
+								</div>
+							</li>
+							<li class="list-group-item">3x4
+								<div class="input-group">
+								<span class="input-group-text">Quantity</span>
+								<input type="number" aria-label="Quantity" class="form-control"/>
+								</div>
+							</li>
+							<li class="list-group-item">4x4
+								<div class="input-group">
+								<span class="input-group-text">Quantity</span>
+								<input type="number" aria-label="Quantity" class="form-control"/>
+								</div>
+							</li>
 						</ul>
 					</div>
 				</div>
 			</div>
 			<div class="accordion-item">
     			<h2 class="accordion-header" id="headingOne">
-      			<button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+      			<button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="true" aria-controls="collapseTwo">
         			Stone Walls
       			</button>
     			</h2>
-				<div id="collapseOne" class="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
+				<div id="collapseTwo" class="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
 					<div class="accordion-body">
 						<ul class="list-group">
-							<li class="list-group-item">2x2 <div class="input-group">
-  							<span class="input-group-text">Quantity</span>
-  							<input type="number" aria-label="Quantity" class="form-control"/>
-							</div></li>
+							<li class="list-group-item">2x2 
+								<div class="input-group">
+								<span class="input-group-text">Quantity</span>
+								<input type="number" aria-label="Quantity" class="form-control"/>
+								</div>
+							</li>
 							<li class="list-group-item">3x3</li>
 							<li class="list-group-item">4x4</li>
 							<li class="list-group-item">2x6</li>
