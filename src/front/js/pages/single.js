7ecbb8ae69ee0a64d7e4075862527eb96e2837db
bpmurray77 <bpmurray77@gmail.com>
2022-07-26 00:21:09 +0000

    Fixed accordion opening

--- src/front/js/pages/single.js
+++ src/front/js/pages/single.js
@@ -6,108 +6,190 @@ import rigoImageUrl from "../../img/rigo-baby.jpg";
 import "../../styles/home.css";
 import "../../styles/inventory.css";
 
-export const Single = props => {
-	const { store, actions } = useContext(Context);
+export const Single = (props) => {
+  const { store, actions } = useContext(Context);
 
-	return (
-		<div className="parent">
-			<div class="accordion" id="accordionExample">
-  			<div class="accordion-item">
-    			<h2 class="accordion-header" id="headingOne">
-      			<button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-				  <a href="https://www.thingiverse.com/thing:171315/files">Stone Dungeon Floor</a>
-      			</button>
-    			</h2>
-				<div id="collapseOne" class="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
-					<div class="accordion-body">
-						<ul class="list-group">
-							<li class="list-group-item">1x1
-								<div class="input-group">
-								<span class="input-group-text">Quantity</span>
-								<input type="number" aria-label="Quantity" class="form-control"/>
-								</div>
-							</li>
-							<li class="list-group-item">1x2
-								<div class="input-group">
-								<span class="input-group-text">Quantity</span>
-								<input type="number" aria-label="Quantity" class="form-control"/>
-								</div>
-							</li>
-							<li class="list-group-item">1x3
-								<div class="input-group">
-								<span class="input-group-text">Quantity</span>
-								<input type="number" aria-label="Quantity" class="form-control"/>
-								</div>
-							</li>
-							<li class="list-group-item">1x4
-								<div class="input-group">
-								<span class="input-group-text">Quantity</span>
-								<input type="number" aria-label="Quantity" class="form-control"/>
-								</div>
-							</li>
-							<li class="list-group-item">2x2
-								<div class="input-group">
-								<span class="input-group-text">Quantity</span>
-								<input type="number" aria-label="Quantity" class="form-control"/>
-								</div>
-							</li>
-							<li class="list-group-item">2x3
-								<div class="input-group">
-								<span class="input-group-text">Quantity</span>
-								<input type="number" aria-label="Quantity" class="form-control"/>
-								</div>
-							</li>
-							<li class="list-group-item">2x4
-								<div class="input-group">
-								<span class="input-group-text">Quantity</span>
-								<input type="number" aria-label="Quantity" class="form-control"/>
-								</div>
-							</li>
-							<li class="list-group-item">3x3
-								<div class="input-group">
-								<span class="input-group-text">Quantity</span>
-								<input type="number" aria-label="Quantity" class="form-control"/>
-								</div>
-							</li>
-							<li class="list-group-item">3x4
-								<div class="input-group">
-								<span class="input-group-text">Quantity</span>
-								<input type="number" aria-label="Quantity" class="form-control"/>
-								</div>
-							</li>
-							<li class="list-group-item">4x4
-								<div class="input-group">
-								<span class="input-group-text">Quantity</span>
-								<input type="number" aria-label="Quantity" class="form-control"/>
-								</div>
-							</li>
-						</ul>
-					</div>
-				</div>
-			</div>
-			<div class="accordion-item">
-    			<h2 class="accordion-header" id="headingOne">
-      			<button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="true" aria-controls="collapseTwo">
-        			Stone Walls
-      			</button>
-    			</h2>
-				<div id="collapseTwo" class="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
-					<div class="accordion-body">
-						<ul class="list-group">
-							<li class="list-group-item">2x2 
-								<div class="input-group">
-								<span class="input-group-text">Quantity</span>
-								<input type="number" aria-label="Quantity" class="form-control"/>
-								</div>
-							</li>
-							<li class="list-group-item">3x3</li>
-							<li class="list-group-item">4x4</li>
-							<li class="list-group-item">2x6</li>
-							<li class="list-group-item">6x6</li>
-						</ul>					</div>
-				</div>
-			</div>
-			</div>
-		</div>
-	);
+  return (
+    <div className="parent">
+      <div class="accordion" id="accordionExample">
+        <div class="accordion-item">
+          <h2 class="accordion-header" id="headingOne">
+            <button
+              class="accordion-button"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#collapseOne"
+              aria-expanded="false"
+              aria-controls="collapseOne"
+            >
+              <a href="https://www.thingiverse.com/thing:171315/files">
+                Stone Dungeon Floor
+              </a>
+            </button>
+          </h2>
+          <div
+            id="collapseOne"
+            class="accordion-collapse collapse"
+          >
+            <div class="accordion-body">
+              <ul class="list-group">
+                <li class="list-group-item">
+                  1x1
+                  <div class="input-group">
+                    <span class="input-group-text">Quantity</span>
+                    <input
+                      type="number"
+                      aria-label="Quantity"
+                      class="form-control"
+                    />
+                  </div>
+                </li>
+                <li class="list-group-item">
+                  1x2
+                  <div class="input-group">
+                    <span class="input-group-text">Quantity</span>
+                    <input
+                      type="number"
+                      aria-label="Quantity"
+                      class="form-control"
+                    />
+                  </div>
+                </li>
+                <li class="list-group-item">
+                  1x3
+                  <div class="input-group">
+                    <span class="input-group-text">Quantity</span>
+                    <input
+                      type="number"
+                      aria-label="Quantity"
+                      class="form-control"
+                    />
+                  </div>
+                </li>
+                <li class="list-group-item">
+                  1x4
+                  <div class="input-group">
+                    <span class="input-group-text">Quantity</span>
+                    <input
+                      type="number"
+                      aria-label="Quantity"
+                      class="form-control"
+                    />
+                  </div>
+                </li>
+                <li class="list-group-item">
+                  2x2
+                  <div class="input-group">
+                    <span class="input-group-text">Quantity</span>
+                    <input
+                      type="number"
+                      aria-label="Quantity"
+                      class="form-control"
+                    />
+                  </div>
+                </li>
+                <li class="list-group-item">
+                  2x3
+                  <div class="input-group">
+                    <span class="input-group-text">Quantity</span>
+                    <input
+                      type="number"
+                      aria-label="Quantity"
+                      class="form-control"
+                    />
+                  </div>
+                </li>
+                <li class="list-group-item">
+                  2x4
+                  <div class="input-group">
+                    <span class="input-group-text">Quantity</span>
+                    <input
+                      type="number"
+                      aria-label="Quantity"
+                      class="form-control"
+                    />
+                  </div>
+                </li>
+                <li class="list-group-item">
+                  3x3
+                  <div class="input-group">
+                    <span class="input-group-text">Quantity</span>
+                    <input
+                      type="number"
+                      aria-label="Quantity"
+                      class="form-control"
+                    />
+                  </div>
+                </li>
+                <li class="list-group-item">
+                  3x4
+                  <div class="input-group">
+                    <span class="input-group-text">Quantity</span>
+                    <input
+                      type="number"
+                      aria-label="Quantity"
+                      class="form-control"
+                    />
+                  </div>
+                </li>
+                <li class="list-group-item">
+                  4x4
+                  <div class="input-group">
+                    <span class="input-group-text">Quantity</span>
+                    <input
+                      type="number"
+                      aria-label="Quantity"
+                      class="form-control"
+                    />
+                  </div>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+        <div class="accordion-item">
+          <h2 class="accordion-header" id="headingOne">
+            <button
+              class="accordion-button"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#collapseTwo"
+              aria-expanded="false"
+              aria-controls="collapseTwo"
+            >
+				<a href="https://www.thingiverse.com/thing:178621/files">
+                Stone Dungeon Walls
+              	</a>
+            </button>
+          </h2>
+          <div
+            id="collapseTwo"
+            class="accordion-collapse collapse"
+            aria-labelledby="headingOne"
+            data-bs-parent="#accordionExample"
+          >
+            <div class="accordion-body">
+              <ul class="list-group">
+                <li class="list-group-item">
+                  1x1
+                  <div class="input-group">
+                    <span class="input-group-text">Quantity</span>
+                    <input
+                      type="number"
+                      aria-label="Quantity"
+                      class="form-control"
+                    />
+                  </div>
+                </li>
+                <li class="list-group-item">3x3</li>
+                <li class="list-group-item">4x4</li>
+                <li class="list-group-item">2x6</li>
+                <li class="list-group-item">6x6</li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
